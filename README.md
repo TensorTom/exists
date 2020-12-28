c120e0f11825530ec04e09d8408eb3938170ff67
TensorTom <14287229+TensorTom@users.noreply.github.com>
2020-12-28 16:25:08 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# exists
+
+Python module to check if a variable or other object exists.
+
+```python
+from exists import exists
+
+myvar = True
+assert exists(myvar)
+```
---
+++ exists/__init__.py
@@ -0,0 +1,18 @@
+from typing import AnyStr, Optional, Union, Literal
+from datafunc import listlike
+
+
+def exists(var: Union[AnyStr], scope: Optional[str] = 'all') -> bool:
+	"""
+	Check for existence of a variable by name.
+	:param var: The string representation/name of the variable (Required).
+	:param scope: Where to look. Defaults to 'all'/local + global (Optional).
+	:return: Boolean - True if var exists, False if not exist.
+	"""
+	_scope = scope.lower()
+	if _scope in ('any', 'local') and var in locals():
+		return True
+	elif _scope in ('any', 'global') and var in globals():
+		return True
+	else:
+		return False
