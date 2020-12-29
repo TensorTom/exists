9bd5eec872b0eb6d7293e87cf9646f7ec9466ebc
TensorTom <14287229+TensorTom@users.noreply.github.com>
2020-12-29 08:56:17 -0500

    Reset version & minor refactor.

--- exists/__init__.py
+++ exists/__init__.py
@@ -6,13 +6,10 @@ def exists(var: Union[AnyStr], scope: Optional[str] = 'any') -> bool:
 	"""
 	Check for existence of a variable by name.
 	:param var: The string representation/name of the variable (Required).
-	:param scope: Where to look. Defaults to 'all'/local + global (Optional).
+	:param scope: Where to look. Defaults to 'any'/local + global (Optional).
 	:return: Boolean - True if var exists, False if not exist.
 	"""
 	_scope = scope.lower()
-	if _scope in ('any', 'local') and var in locals():
+	if _scope in ('any', 'global') and var in globals():
 		return True
-	elif _scope in ('any', 'global') and var in globals():
-		return True
-	else:
-		return False
+	return _scope in ('any', 'local') and var in locals()
